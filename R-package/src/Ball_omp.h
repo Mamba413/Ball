6a6046ac1e75cfa3a932b88c8587f1d7f13477e2
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2021-09-20 15:22:21 +0800

    Update Ball_omp.h
    Remove the warning message when OpenMP is not available.

--- R-package/src/Ball_omp.h
+++ R-package/src/Ball_omp.h
@@ -3,12 +3,6 @@
 #if defined(_OPENMP)
 #include <omp.h>
 #else
-#ifndef DISABLE_OPENMP
-// use pragma message instead of warning
-#pragma message("Warning: OpenMP is not available, "                    \
-"project will be compiled into single-thread code. "                    \
-"Use OpenMP-enabled compiler to get benefit of multi-threading.")
-#endif
 static inline int omp_get_thread_num() { return 0; }
 static inline int omp_get_num_threads() { return 1; }
 static inline int omp_get_num_procs() { return 1; }
