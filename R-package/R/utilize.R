4be598945fc654add81650842da3669869665e9a
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2022-09-03 17:28:04 +0800

    Update utilize.R

--- R-package/R/utilize.R
+++ R-package/R/utilize.R
@@ -12,7 +12,6 @@ BD_WEIGHT_STATS <- c("bd.constant", "bd.variance", "bd.rbf")
 KBD_WEIGHT_STATS <- c("kbd.sum.constant", "kbd.sum.variance", "kbd.max.constant", "kbd.max.variance", "kbd.maxsum.constant", "kbd.maxsum.variance")
 
 center_bdd_matrix <- function(bdd) {
-  num <- dim(bdd)[1]
   bdd <- sweep(bdd, 2, colMeans(bdd)) - rowMeans(bdd) + mean(bdd)
   bdd
 }
